c9314daf7ef23157151398d1f1274e52e82f2c5c
GohCheeWee <cheeweegoh1997@gmail.com>
2019-07-31 22:37:31 +0800

    10.35

--- admin/src/main/java/com/jby/admin/MainActivity.java
+++ admin/src/main/java/com/jby/admin/MainActivity.java
@@ -56,7 +56,7 @@ import static com.jby.admin.shareObject.VariableUtils.REFRESH_STOCK_LIST;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         StockFragment.OnFragmentInteractionListener, RemarkFragment.OnFragmentInteractionListener,
         View.OnClickListener, CustomerDialog.CustomerDialogCallBack {
-    //new version
+    //new version22
     //navigation drawer
     private DrawerLayout mainActivityDrawerLayout;
     private ActionBarDrawerToggle mainActivityActionBarDrawerToggle;
