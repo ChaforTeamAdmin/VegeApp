b0d020f64892639cc3f7eafd490b0e84831035da
GohCheeWee <cheeweegoh1997@gmail.com>
2019-07-31 22:04:07 +0800

    new activity

---
+++ admin/src/main/java/com/jby/admin/testing.java
@@ -0,0 +1,13 @@
+package com.jby.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testing extends AppCompatActivity {
+    //lol
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
